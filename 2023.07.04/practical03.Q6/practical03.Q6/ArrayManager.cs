fc8d23bac947d9e380b0b11c983a7cd43200469a
amandiumesha <136356239+amandiumesha@users.noreply.github.com>
2023-07-04 10:00:05 +0530

    Add files via upload

---
+++ 2023.07.04/practical03.Q6/practical03.Q6/ArrayManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical03.Q6
+{
+    internal class ArrayManager
+    {
+        public int[] CreateArray(int size)
+        {
+            return new int[size * 2];
+        }
+
+        public void SetValue(int[] array, int index, int value)
+        {
+            array[index * 2] = value;
+            array[(index * 2) + 1] = 0;
+        }
+    }
+}
---
+++ 2023.07.04/practical03.Q6/practical03.Q6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical03.Q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size = int.Parse(Console.ReadLine());
+
+            ArrayManager arrayManager = new ArrayManager();
+            int[] array = arrayManager.CreateArray(size);
+
+            Console.WriteLine("Enter values for the array:");
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter value: ");
+                int value = int.Parse(Console.ReadLine());
+
+                arrayManager.SetValue(array, i, value);
+            }
+
+            Console.WriteLine("Array values with zeros:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+        }
+    }
+
